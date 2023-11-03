823fc5d91ebc88a75a647c9578bcfae4ff5361f0
TOLOES <tomdehame@orange.fr>
2023-11-03 15:16:48 +0100

    Exo2

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -1,1 +1,23 @@
 <?php
+
+// src/Controller/ClientController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClientController
+{
+    /**
+     * @Route("/client/prenom/{prenom}", name="client_info", requirements={"prenom"="[a-zA-Z]+(?:-[a-zA-Z]+)*-*"})
+     */
+    public function info(string $prenom): Response
+    {
+        if (substr($prenom, -1) === '-' || $prenom[0] === '-') {
+            return new Response("Le prénom ne peut pas commencer ou se terminer par un tiret.");
+        }
+
+        return new Response("Prénom : " . htmlspecialchars($prenom));
+    }
+}
