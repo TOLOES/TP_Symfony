39bc28c56bd1ca9b2f3f31a8a041816833d09d76
TOLOES <tomdehame@orange.fr>
2023-11-23 12:08:56 +0100

    Exo 2

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -7,6 +7,8 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
+
 
 class ImageController extends AbstractController
 {
@@ -16,6 +18,22 @@ class ImageController extends AbstractController
         return $this->render('img/home.html.twig', [
             'title' => 'Site Image',
         ]);
+    }
+    #[Route('/img/data/{imageName}', name: 'image_data')]
+    public function affiche(string $imageName): Response
+    {
+        $imagePath = $this->getParameter('kernel.project_dir') . '/images/' . $imageName . '.jpeg';
+
+        if (!file_exists($imagePath)) {
+            throw $this->createNotFoundException('L\'image demandée n\'existe pas.');
+        }
+
+        $file = new File($imagePath);
+        $response = new Response(file_get_contents($imagePath));
+        $response->headers->set('Content-Type', 'image/jpeg');
+        $response->headers->set('Content-Disposition', 'inline; filename="' . $file->getFilename() . '"');
+
+        return $response;
     }
 }
 
